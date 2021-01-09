a4ad51a86db5f0be86c98cdc2e3f89b04029265e
gogsi <dragutinovic.g@gmail.com>
2021-01-09 13:19:25 -0800

    "Added Comment Posting"

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -97,6 +97,25 @@ export const addCampsites = campsites => ({
 
 //COMMENTS
 
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+         campsiteId: campsiteId,
+         rating: rating,
+         author: author,
+         text: text
+     };
+     newComment.date = new Date().toISOString();
+
+     setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+}
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
 export const fetchComments = () => dispatch => {    
     return fetch(baseUrl + 'comments')
         .then(response => {
@@ -128,46 +147,46 @@ export const addComments = comments => ({
     payload: comments
 });
 
-export const addComment = comment => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: comment
-});
+// export const addComment = comment => ({
+//     type: ActionTypes.ADD_COMMENT,
+//     payload: comment
+// });
 
-export const postComment = (campsiteId, rating, author, text) => dispatch => {
-    const newComment = {
-        campsiteId: campsiteId,
-        rating: rating,
-        author: author,
-        text: text
-    };
-    newComment.date = new Date().toISOString();
+// export const postComment = (campsiteId, rating, author, text) => dispatch => {
+//     const newComment = {
+//         campsiteId: campsiteId,
+//         rating: rating,
+//         author: author,
+//         text: text
+//     };
+//     newComment.date = new Date().toISOString();
 
-    return fetch(baseUrl + 'comments', {
-            method: "POST",
-            body: JSON.stringify(newComment),
-            headers: {
-                "Content-Type": "application/json"
-            }
-    })
-    .then(response => {
-            if (response.ok) {
-                return response;
-            } else {
-                const error = new Error(`Error  ${response.status}: ${response.statusText}`);
-                error.response = response;
-                throw error;
-            }
-        },
-        error => { throw error; }
-    )
-        .then(response => response.json())
-        .then(response => dispatch(addComment(response)))
-        .catch(error => {
-            console.log('post comment', error.message);
-            alert('Your comment could not be posted \n Error: ' + error.message)
-        })
+//     return fetch(baseUrl + 'comments', {
+//             method: "POST",
+//             body: JSON.stringify(newComment),
+//             headers: {
+//                 "Content-Type": "application/json"
+//             }
+//     })
+//     .then(response => {
+//             if (response.ok) {
+//                 return response;
+//             } else {
+//                 const error = new Error(`Error  ${response.status}: ${response.statusText}`);
+//                 error.response = response;
+//                 throw error;
+//             }
+//         },
+//         error => { throw error; }
+//     )
+//         .then(response => response.json())
+//         .then(response => dispatch(addComment(response)))
+//         .catch(error => {
+//             console.log('post comment', error.message);
+//             alert('Your comment could not be posted \n Error: ' + error.message)
+//         })
 
-};
+// };
 
 //PROMOTIONS
 
